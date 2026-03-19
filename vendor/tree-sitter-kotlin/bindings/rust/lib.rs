0658970c8967ed9f649457af499338c1e3f10037
Bruno Meilick <b@bnomei.com>
2026-03-19 13:42:41 +0000

    vendor

---
+++ vendor/tree-sitter-kotlin/bindings/rust/lib.rs
@@ -0,0 +1,52 @@
+//! This crate provides kotlin language support for the [tree-sitter][] parsing library.
+//!
+//! Typically, you will use the [LANGUAGE][] constant to add this language to a
+//! tree-sitter [Parser][], and then use the parser to parse some code:
+//!
+//! ```
+//! use tree_sitter::Parser;
+//!
+//! let code = "";
+//! let mut parser = Parser::new();
+//! parser
+//!     .set_language(&tree_sitter_kotlin::LANGUAGE.into())
+//!     .expect("Error loading kotlin grammar");
+//! let tree = parser.parse(code, None).unwrap();
+//! ```
+//!
+//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [tree-sitter]: https://tree-sitter.github.io/
+
+use tree_sitter_language::LanguageFn;
+
+extern "C" {
+    fn tree_sitter_kotlin() -> *const ();
+}
+
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
+///
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_kotlin) };
+
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
+
+// Uncomment these to include any queries that this grammar contains
+
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
+// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
+// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_can_load_grammar() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE.into())
+            .expect("Error loading kotlin language");
+    }
+}
